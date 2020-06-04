a80613d067b806caf75d53cc313bf0e21ead1e2a
Alejandro Gálvez <>
2020-06-04 13:32:58 +0200

    Finished TennisGame1 refactor.

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -1,54 +1,51 @@
 package tennis
 
 
-class TennisGame1 (val player1Name : String, val player2Name : String) extends TennisGame {
-  var m_score1: Int = 0
-  var m_score2: Int = 0
+class TennisGame1(val player1Name: String, val player2Name: String) extends TennisGame {
+    val scoresString = Map(0 -> "Love", 1 -> "Fifteen", 2 -> "Thirty", 3 -> "Forty")
 
-  def wonPoint(playerName : String) {
-          if (playerName == "player1")
-              m_score1 += 1
-          else
-              m_score2 += 1
-      }
+    var score1: Int = 0
+    var score2: Int = 0
 
-  def calculateScore() : String = {
-      var score : String = ""
-      var tempScore=0
-      if (m_score1==m_score2)
-      {
-        score = m_score1 match {
-              case 0 =>  "Love-All"
-              case 1 => "Fifteen-All"
-              case 2 => "Thirty-All"
-              case _ => "Deuce"
+    def wonPoint(playerName: String) {
+        if (playerName == "player1") score1 += 1
+        else score2 += 1
+    }
 
-          }
-      }
-      else if (m_score1>=4 || m_score2>=4)
-      {
-          val minusResult = m_score1-m_score2
-          if (minusResult==1) score ="Advantage player1"
-          else if (minusResult == -1) score ="Advantage player2"
-          else if (minusResult>=2) score = "Win for player1"
-          else score ="Win for player2"
-      }
-      else
-      {
-          for ( i<- 1 until 3 by 1)
-          {
-              if (i==1) tempScore = m_score1
-              else { score+="-"; tempScore = m_score2;}
-              val tempScore2 = tempScore match {
-                  case 0 => "Love"
-                  case 1 => "Fifteen"
-                  case 2 => "Thirty"
-                  case 3 => "Forty"
-              }
-            score += tempScore2
-          }
-      }
-    return score
-  }
+    def calculateScore(): String = {
+        val score: String = ""
 
+        score match {
+            case _ if score1 == score2 => handleDraw(score1)
+            case _ if score1 >= 4 || score2 >= 4 => handleAdvantage(score1 - score2)
+            case _ => getScoreString(score1) + "-" + getScoreString(score2)
+        }
+    }
+
+    def handleDraw(score: Int): String = {
+        score match {
+            case 0 => "Love-All"
+            case 1 => "Fifteen-All"
+            case 2 => "Thirty-All"
+            case _ => "Deuce"
+        }
+    }
+
+    def handleAdvantage(difference: Int): String = {
+        difference match {
+            case -1 => "Advantage player2"
+            case 1 => "Advantage player1"
+            case _ if difference >= 2 => "Win for player1"
+            case _ => "Win for player2"
+        }
+    }
+
+    def getScoreString(score: Int): String = {
+        score match {
+            case 0 => "Love"
+            case 1 => "Fifteen"
+            case 2 => "Thirty"
+            case 3 => "Forty"
+        }
+    }
 }
